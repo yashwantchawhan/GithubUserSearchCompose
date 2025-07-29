1fa250561639e3990476b7ea5e818f78d1f5eecc
YASHWANT CHAWHAN <ynchawhan4@gmail.com>
2025-07-29 12:17:04 +0400

    Adding design system

--- core/design-system/build.gradle.kts
+++ core/design-system/build.gradle.kts
@@ -2,12 +2,10 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp")
-    id("com.google.dagger.hilt.android")
 }
 
 android {
-    namespace = "com.example.feature.user_list"
+    namespace = "com.example.core.esign_system"
     compileSdk = 35
 
     defaultConfig {
@@ -40,10 +38,6 @@ android {
 }
 
 dependencies {
-
-    //Hilt
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
